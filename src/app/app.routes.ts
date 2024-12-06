7aa7b50524a65037b61f7bd083c969d2eab2a62a
Rajadurai Sermaraj <Rajadurai.Sermaraj@kanini.com>
2024-12-06 15:53:06 +0530

    personal info stepper done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { PersonalInfoComponent } from './personal-info/personal-info.component';
+import { PlanComponent } from './plan/plan.component';
+import { AddOnsComponent } from './add-ons/add-ons.component';
+import { SummaryComponent } from './summary/summary.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [  
+  { path: 'personalinfo', component: PersonalInfoComponent },
+  { path: 'plan', component: PlanComponent },
+  { path: 'addons', component: AddOnsComponent },
+  { path: 'summary', component: SummaryComponent },
+  { path: '', redirectTo: 'personalinfo', pathMatch: 'full' },
+  { path: '**', redirectTo: 'personalinfo' }
+];
