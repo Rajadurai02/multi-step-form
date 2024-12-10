8dd4caa398d27f8ffdbb9347e3a36892f63aa76f
Rajadurai Sermaraj <Rajadurai.Sermaraj@kanini.com>
2024-12-10 17:30:24 +0530

    summary and thank you component added

--- src/app/summary/summary.component.ts
+++ src/app/summary/summary.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { FormStepperComponent } from '../form-stepper/form-stepper.component';
+import { FormHeaderComponent } from '../form-header/form-header.component';
+import { ThankYouComponent } from '../thank-you/thank-you.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-summary',
   standalone: true,
-  imports: [],
+  imports: [FormStepperComponent, FormHeaderComponent, ThankYouComponent, CommonModule],
   templateUrl: './summary.component.html',
   styleUrl: './summary.component.css'
 })
 export class SummaryComponent {
-
+  headerTitle:string = "Finishing up";
+  headerInfo:string = "Double-check everything looks OK before confirming.";
+  stepperCount: number = 4;
+  isFormSubmitted: boolean = false;
 }
